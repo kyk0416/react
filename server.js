43aed78dd9b3957109dc82920a0a131c043fd25f
LAPTOP-BRJQG4NS\ykkim <kyk0416@gmail.com>
2020-03-08 21:45:36 +0700

    create customer database table & node js connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,32 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birth': '991231',
-            'gender': '남',
-            'job': '학생'
-            },
-            {
-              'id': 2,
-              'image': 'https://placeimg.com/64/64/2',
-              'name': '가나다',
-              'birth': '991131',
-              'gender': '남',
-              'job': '학생'
-            },
-            {
-              'id': 3,
-              'image': 'https://placeimg.com/64/64/3',
-              'name': '홍길동2',
-              'birth': '991222',
-              'gender': '남',
-              'job': '학생'
-            }
-    ]);
-})
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
