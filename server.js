1624bc6067e20def548f451464ebb89a38bd1cb9
LAPTOP-BRJQG4NS\ykkim <kyk0416@gmail.com>
2020-03-09 07:39:17 +0700

    implement customer add moudle & refresh

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'});
 
 app.get('/api/customers', (req, res) => {
     connection.query(
@@ -29,5 +31,22 @@ app.get('/api/customers', (req, res) => {
         }
     );
 });
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birth = req.body.birth;
+    let gender = req.body.gender;
+    let job = req.body.job;
+
+    let params = [image, name, birth, gender, job];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        })    
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
